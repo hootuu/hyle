ee01c7dd49dd5eb1415c66abae4d7c46018ae2dc
UNIVN <valuable.network.lz@gmail.com>
2025-05-04 01:12:11 +0800

    init the hyle

--- go.mod
+++ go.mod
@@ -11,6 +11,7 @@ require (
 )
 
 require (
+	github.com/avast/retry-go v3.0.0+incompatible // indirect
 	github.com/fsnotify/fsnotify v1.8.0 // indirect
 	github.com/go-viper/mapstructure/v2 v2.2.1 // indirect
 	github.com/patrickmn/go-cache v2.1.0+incompatible // indirect
---
+++ hretry/retry.go
@@ -0,0 +1,22 @@
+package hretry
+
+import (
+	"github.com/avast/retry-go"
+	"github.com/hootuu/hyle/hcfg"
+	"github.com/hootuu/hyle/hlog"
+	"github.com/spf13/cast"
+	"go.uber.org/zap"
+	"time"
+)
+
+func Universal(call func() error) {
+	err := retry.Do(func() error {
+		return call()
+	},
+		retry.Attempts(cast.ToUint(hcfg.GetInt("retry.universal.attempts", 3))),
+		retry.Delay(hcfg.GetDuration("retry.universal.delay", 600*time.Millisecond)),
+	)
+	if err != nil {
+		hlog.Err("hyle.retry.Universal", zap.Error(err))
+	}
+}
