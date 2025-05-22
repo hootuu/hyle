71bbfd5f3bbfefd9eb86a93d7ad8c23b62d6f6e7
UNIVN <valuable.network.lz@gmail.com>
2025-05-22 19:15:00 +0800

    fast state machine

---
+++ hfsm/fsm.go
@@ -0,0 +1,51 @@
+package hfsm
+
+import (
+	"context"
+	"fmt"
+	"github.com/hootuu/hyle/data/dict"
+	"sync"
+)
+
+type Machine struct {
+	transitions map[State]map[Event]Transition
+	mu          sync.RWMutex
+}
+
+func NewMachine() *Machine {
+	return &Machine{
+		transitions: make(map[State]map[Event]Transition),
+	}
+}
+
+func (m *Machine) AddTransition(current State, event Event, trans Transition) {
+	m.mu.Lock()
+	defer m.mu.Unlock()
+
+	if _, ok := m.transitions[current]; !ok {
+		m.transitions[current] = make(map[Event]Transition)
+	}
+
+	m.transitions[current][event] = trans
+}
+
+func (m *Machine) Handle(ctx context.Context, current State, event Event, data dict.Dict) (State, error) {
+	m.mu.Lock()
+	transitions, ok := m.transitions[current]
+	if !ok {
+		m.mu.Unlock()
+		return current, fmt.Errorf("no transitions defined for state: %d", current)
+	}
+	trans, ok := transitions[event]
+	if !ok {
+		m.mu.Unlock()
+		return current, fmt.Errorf("no transition for event %d in state %d", event, current)
+	}
+	m.mu.Unlock()
+
+	target, err := trans(ctx, current, event, data)
+	if err != nil {
+		return current, err
+	}
+	return target, nil
+}
---
+++ hfsm/types.go
@@ -0,0 +1,12 @@
+package hfsm
+
+import (
+	"context"
+	"github.com/hootuu/hyle/data/dict"
+)
+
+type State int
+
+type Event int
+
+type Transition func(ctx context.Context, currentState State, event Event, data dict.Dict) (State, error)
